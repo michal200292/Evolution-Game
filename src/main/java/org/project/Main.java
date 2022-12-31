37d9f2c3b05b315db0010e785129595eec23f6ff
Michał Nożkiewicz <michno513@gmail.com>
2022-12-31 17:46:35 +0100

    Writing to file

--- src/main/java/org/project/Main.java
+++ src/main/java/org/project/Main.java
@@ -5,9 +5,10 @@ package org.project;
 import javafx.application.Application;
 import org.project.gui.App;
 
+import java.util.Arrays;
 
 public class Main {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         Application.launch(App.class, args);
     }
 }
